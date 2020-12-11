d58624f90415f6d6e22f286991e8b637766ac8d1
Medha Gupta <66296461+medha-3102@users.noreply.github.com>
2020-12-11 23:59:40 +0530

    Algo

---
+++ Bits Manipulation/firstNonRepeating.java
@@ -0,0 +1,36 @@
+package bitsManipulation;
+import java.util.*;
+public class FirstNonRepeating {
+	// Efficient Java program to find first non- 
+	// repeating element.
+
+		static int firstNonRepeating(int arr[], int n) 
+		{ 
+			// Insert all array elements in hash 
+			// table 
+
+			Map<Integer, Integer> m = new HashMap<>(); 
+			for (int i = 0; i < n; i++) { 
+				if (m.containsKey(arr[i])) { 
+					m.put(arr[i], m.get(arr[i]) + 1); 
+				} 
+				else { 
+					m.put(arr[i], 1); 
+				} 
+			} 
+			// Traverse array again and return 
+			// first element with count 1. 
+			for (int i = 0; i < n; i++) 
+				if (m.get(arr[i]) == 1) 
+					return arr[i]; 
+			return -1; 
+		} 
+
+		// Driver code 
+		public static void main(String[] args) 
+		{ 
+			int arr[] = { 9, 4, 9, 6, 7, 4 }; 
+			int n = arr.length; 
+			System.out.println(firstNonRepeating(arr, n)); 
+		} 
+	} 
