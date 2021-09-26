962f5afb35c940957bebfcdd8934b6e798955657
Medha Gupta <66296461+medha-3102@users.noreply.github.com>
2021-09-26 19:24:55 +0530

    Search in sorted & rotated array via binary search

---
+++ searching/SortedRotatedArray.java
@@ -0,0 +1,42 @@
+package searching;
+
+import java.util.Scanner;
+
+public class SortedRotatedArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++) {
+			a[i]=sc.nextInt();
+		}
+		int s=sc.nextInt();
+		int index=func(a,s);
+		if(index==-1)
+			System.out.println("Not found!");
+		else
+		System.out.println("Found at pos "+(index+1));
+	}
+	static int func(int a[],int s) {
+		int low=0,high=a.length-1;
+		while(low<=high) {
+			int mid = (low+high)/2;
+			if(a[mid]==s) return mid;
+			if(a[low]<a[mid]) {
+				if(s>=a[low]&&s<a[mid])
+					high=mid-1;
+				else
+					low=mid+1;
+			}else {
+			if(s>a[mid]&&s<=a[high])
+				low=mid+1;
+			else
+				high=mid-1;
+			}
+		}
+		return -1;
+	}
+
+}
