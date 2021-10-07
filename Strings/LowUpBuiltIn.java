f7258218b6f16e857547bf572557d53e65a0fe70
Medha Gupta <66296461+medha-3102@users.noreply.github.com>
2021-10-07 19:25:18 +0530

    Change in lower and uppercase with built in func.

---
+++ Strings/LowUpBuiltIn.java
@@ -0,0 +1,13 @@
+package strings;
+
+public class LowUpBuiltIn {
+
+	public static void main(String[] args) {
+		String s ="medha";
+		System.out.println(s.toUpperCase());
+		System.out.println(s.toLowerCase());
+		// TODO Auto-generated method stub
+
+	}
+
+}
