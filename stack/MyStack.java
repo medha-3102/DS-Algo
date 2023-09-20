7ab9992471992896413641d15c2a8bd9cb8b41b2
Medha Gupta <66296461+medha-3102@users.noreply.github.com>
2023-09-21 00:19:59 +0530

    Stack implementation from scratch

--- stack/MyStack.java
+++ stack/MyStack.java
@@ -1,23 +1,51 @@
-package stack;
+package college;
 
-import linkedList.MyLinkedList;
+import java.util.Scanner;
 
-public class MyStack<E> {
-	private MyLinkedList<E> ll=new MyLinkedList<>();
-	
-
-	void push(E e) {
-		ll.add(e);
-	}
-
-	E pop() throws Exception {
-		if(ll.isEmpty()) {
-			throw new Exception("Popping from empty stack is not allowed");
+class NewStack{
+	int top=-1;
+	int a[] = new int[10];
+	 void push(Scanner sc) {
+		if(top==9)
+			System.out.println("Overflow");
+		else {
+			System.out.println("Enter an element");
+			int ele = sc.nextInt();
+			top++;
+			a[top]=ele;
+				
 		}
-		return ll.removeLast();
 	}
-	E peek()throws Exception {
-		return ll.getLast();
+	 void pop() {
+		 if(top==-1)
+			 System.out.println("Empty");
+		 else {
+			 System.out.println(a[top]+" is popped.");top--;
+		 }
+	 }
+	 void print() {
+		 for(int i=0;i<=top;i++)
+			 System.out.print(a[i]+" ");
+	 }
+}
+
+
+public class MyStack {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		NewStack ns = new NewStack();
+		ns.push(sc);
+		ns.pop();
+		ns.push(sc);
+		ns.push(sc);
+		ns.pop();
+		ns.push(sc);
+		ns.push(sc);
+		ns.print();
 		
+		// TODO Auto-generated method stub
+
 	}
+
 }
